7f4c7f2e092bdcd075bb1407b41a76ff144dfcfb
Peter Zastoupil <peterzastoupil@gmail.com>
2016-09-13 09:15:47 -0500

    updated `groupBy` to be implemented in terms of `zipMap`

--- src/lib/combinators/groupBy.js
+++ src/lib/combinators/groupBy.js
@@ -1,11 +1,12 @@
 import { map } from './map'
 import { dedupe } from './dedupe'
+import { zipMap } from './zipMap'
 import { duplicates } from './duplicates'
 
 export const groupBy = (iterable, toKey) =>
-  map(
+  zipMap(
     dedupe(map(iterable, toKey)),
-    (key) => [key, duplicates(iterable, key, toKey)]
+    (key) => duplicates(iterable, key, toKey)
   )
 
 export default groupBy
