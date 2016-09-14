0c9394e9cdfaceb178d41e21e03a773bd5f742b6
Peter Zastoupil <peterzastoupil@gmail.com>
2016-09-14 16:58:51 -0500

    refactored `forEach` to be a materializing combinator

--- src/lib/combinators/index.js
+++ src/lib/combinators/index.js
@@ -1,9 +1,3 @@
+export * from './aggregates'
 export * from './core'
-
-export dedupe from './dedupe'
-export every from './every'
-export forEach from './forEach'
-export groupBy from './groupBy'
-export partition from './partition'
-export reduce from './reduce'
-export some from './some'
+export * from './materializers'
