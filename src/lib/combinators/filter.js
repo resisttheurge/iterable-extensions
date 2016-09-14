ace94fc12c73cae368c17504db9865b9f9cb8d4d
Peter Zastoupil <peterzastoupil@gmail.com>
2016-09-13 23:47:30 -0500

    fixed `filter`combinator
    * was calling the predicate on the iterable
    * now correctly calls the predicate on the current element

--- src/lib/combinators/filter.js
+++ src/lib/combinators/filter.js
@@ -3,7 +3,7 @@ import Iterable from 'Iterable'
 export const filter = (iterable, predicate) =>
   new Iterable(function * () {
     for (let elem of iterable) {
-      if (predicate(iterable)) {
+      if (predicate(elem)) {
         yield elem
       }
     }
